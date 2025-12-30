2775799bd2c6f0cc5d18ac1fc347c030eb2c8119
Bhattvishvesh <bhattvishvesh27@gmail.com>
2025-12-30 08:42:15 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function Temp(){
     let city=document.getElementById("search").value;
-    var api=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4b37bcc9f0afc6e48380ef2d3cc1b78b&units=metric;`
+    if(city==undefined){
+        alert("enter city name");
+    }
+    else{
+    var api=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4b37bcc9f0afc6e48380ef2d3cc1b78b&units=metric`;
     fetch (api)
     .then(function(response){
         return response.json();
@@ -8,8 +12,8 @@ function Temp(){
     .then(function (weatherObj) {
       document.getElementById("cityName").textContent = weatherObj.name + ","
       document.getElementById("country").textContent = weatherObj.sys.country;
-    document.getElementById("temp").innerHTML =`${weatherObj.main.temp}<sup> <sup>o</sup>C</sup>;`
-    document.getElementById("wind").innerHTML = `${weatherObj.wind.speed}<sub>km/h</sub>;`
+    document.getElementById("temp").innerHTML =`${weatherObj.main.temp}<sup> <sup>o</sup>C</sup>`;
+    document.getElementById("wind").innerHTML = `${weatherObj.wind.speed}<sub>km/h</sub>`;
     document.getElementById("cloud").innerHTML =` ${weatherObj.clouds.all}%`;
     document.getElementById("visibility").innerHTML = `${weatherObj.visibility}`;
     document.getElementById("sea").innerHTML=`${weatherObj.main.sea_level}`;
@@ -18,4 +22,30 @@ function Temp(){
 
 
 }
+}
 
+function bhavnagar(){
+    let city="bhavnagar";
+     if(city==undefined){
+        alert("enter city name");
+    }
+    else{
+    var api=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4b37bcc9f0afc6e48380ef2d3cc1b78b&units=metric`;
+    fetch (api)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (weatherObj) {
+      document.getElementById("cityName").textContent = weatherObj.name + ","
+      document.getElementById("country").textContent = weatherObj.sys.country
+    document.getElementById("temp").innerHTML =`${weatherObj.main.temp}<sup> <sup>o</sup>C</sup>`
+    document.getElementById("wind").innerHTML = `${weatherObj.wind.speed}<sub>km/h</sub>`
+    document.getElementById("cloud").innerHTML =` ${weatherObj.clouds.all}%`
+    document.getElementById("visibility").innerHTML = `${weatherObj.visibility}`
+    document.getElementById("sea").innerHTML=`${weatherObj.main.sea_level}`
+        document.getElementById("feels").innerHTML=`${weatherObj.main.feels_like}`
+    })
+
+
+}
+}
